5081c4d8cebb40d32f1d099c41d6a9602eb3babd
Jason Estey <esteyj@yahoo.com>
2010-04-19 17:50:03 -0700

    added bmp2png for .NET

---
+++ dot_net/bmp2png/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace bmp2png
+{
+   /// <summary>
+   /// Glob is used to handle command-line wildcards, such as "*.bmp".
+   /// This is a quick n' dirty solution so be gentle with it. :)
+   /// Big thanks to Brian Ary for providing this code on his blog:
+   /// http://brianary.blogspot.com/2007/09/globbing-using-filename-wildcard.html
+   /// </summary>
+   public static class Glob
+   {
+       static public string[] GetFiles(string[] patterns)
+       {
+           List<string> filelist = new List<string>();
+           foreach (string pattern in patterns)
+               filelist.AddRange(GetFiles(pattern));
+           string[] files = new string[filelist.Count];
+           filelist.CopyTo(files, 0);
+           return files;
+       }
+
+       static public string[] GetFiles(string patternlist)
+       {
+           List<string> filelist = new List<string>();
+           foreach (string pattern in
+               patternlist.Split(Path.GetInvalidPathChars()))
+           {
+               string dir = Path.GetDirectoryName(pattern);
+               if (String.IsNullOrEmpty(dir)) dir =
+                    Directory.GetCurrentDirectory();
+               filelist.AddRange(Directory.GetFiles(
+                   Path.GetFullPath(dir),
+                   Path.GetFileName(pattern)));
+           }
+           string[] files = new string[filelist.Count];
+           filelist.CopyTo(files, 0);
+           return files;
+       }
+   }
+
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           if (args.Length == 0)
+           {
+               Console.WriteLine("usage: bmp2png <filename> ...");
+           }
+
+           foreach (string arg in args)
+           {
+               string[] files = Glob.GetFiles(arg);
+               foreach (string filename in files)
+               {
+                   if (filename.EndsWith(".bmp", true, null))
+                   {
+                       string outfile = filename.Substring(0, filename.Length - 4);
+                       outfile += ".png";
+
+                       Image inputImage = Image.FromFile(filename);
+                       inputImage.Save(outfile, ImageFormat.Png);
+                   }
+                   else
+                   {
+                       Console.WriteLine("warning: {0} is not a .bmp file", filename);
+                   }
+               }
+           }
+       }
+   }
+}
---
+++ dot_net/bmp2png/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bmp2png")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bushido Burrito")]
+[assembly: AssemblyProduct("bmp2png")]
+[assembly: AssemblyCopyright("Copyright © Bushido Burrito 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cfb6dc4-6a2f-4cb2-8aa3-d0c1fd9298e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
