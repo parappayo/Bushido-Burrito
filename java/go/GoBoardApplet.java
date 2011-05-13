d360852ca54ae759e83553cbb83647dad1615103
Jason Estey <esteyj@yahoo.com>
2011-05-13 14:30:52 -0700

    can now place stones, no removal yet

--- java/go/GoBoardApplet.java
+++ java/go/GoBoardApplet.java
@@ -11,7 +11,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class GoBoardApplet extends JApplet
+public class GoBoardApplet extends JApplet implements MouseListener
 {
 	GoBoardComponent board;
 
@@ -25,6 +25,7 @@ public class GoBoardApplet extends JApplet
 		board = new GoBoardComponent();
 		board.whiteStone = loadImage("white_stone.png");
 		board.blackStone = loadImage("black_stone.png");
+		board.addMouseListener(this);
 		add("Center", board);
 	}
 
@@ -36,6 +37,17 @@ public class GoBoardApplet extends JApplet
 		}
 		return null;
 	}
+
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX() / board.getStoneWidth();
+		int y = e.getY() / board.getStoneHeight();
+		board.takeMove(x, y);
+	}
+
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Go Board");
--- java/go/GoBoardComponent.java
+++ java/go/GoBoardComponent.java
@@ -4,24 +4,58 @@ import java.awt.image.*;
 
 public class GoBoardComponent extends Component
 {
+	boolean isBlackTurn;
+
 	int boardWidth = 19;
 	int boardHeight = 19;
 
 	BufferedImage whiteStone = null;
 	BufferedImage blackStone = null;
 
-	public Dimension getPreferredSize() {
+	enum Stone { EMPTY, BLACK, WHITE }
 
-		int stoneWidth = 32;
-		int stoneHeight = 32;
-		if (whiteStone != null) {
-			stoneWidth = whiteStone.getWidth();
-			stoneHeight = whiteStone.getHeight();
+	private Stone[] boardData;
+
+	public GoBoardComponent() {
+		isBlackTurn = true;
+
+		boardData = new Stone[boardWidth * boardHeight];
+		for (int i = 0; i < boardData.length; i++) {
+			boardData[i] = Stone.EMPTY;
 		}
+	}
 
+	public void takeMove(int x, int y) {
+		Stone s = isBlackTurn ? Stone.BLACK : Stone.WHITE;
+		isBlackTurn = !isBlackTurn;
+		setStone(x, y, s);
+		repaint();
+	}
+
+	public void setStone(int x, int y, Stone s) {
+		boardData[y * boardWidth + x] = s;
+	}
+
+	public Stone getStone(int x, int y) {
+		return boardData[y * boardWidth + x];
+	}
+
+	public int getStoneWidth() {
+		int retval = 32;
+		if (whiteStone != null) { retval = whiteStone.getWidth(); }
+		return retval;
+	}
+
+	public int getStoneHeight() {
+		int retval = 32;
+		if (whiteStone != null) { retval = whiteStone.getHeight(); }
+		return retval;
+	}
+
+	public Dimension getPreferredSize() {
 		return new Dimension(
-			boardWidth * stoneWidth,
-			boardHeight * stoneHeight
+			boardWidth * getStoneWidth(),
+			boardHeight * getStoneHeight()
 			);
 	}
 
@@ -31,8 +65,34 @@ public class GoBoardComponent extends Component
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(0, 0, size.width, size.height);
 
-		// for testing only
-		g.drawImage( blackStone, 10, 10, null);
+		int w = getStoneWidth();
+		int h = getStoneHeight();
+
+		g.setColor(Color.BLACK);
+		for (int x = 0; x < boardWidth; x++) {
+			int x1 = x*w + w/2;
+			int y1 = h/2;
+			int y2 = (boardHeight - 1)*h + h/2;
+			g.drawLine(x1, y1, x1, y2);
+		}
+		for (int y = 0; y < boardHeight; y++) {
+			int y1 = y*h + h/2;
+			int x1 = w/2;
+			int x2 = (boardWidth - 1)*w + w/2;
+			g.drawLine(x1, y1, x2, y1);
+		}
+
+		for (int i = 0; i < boardData.length; i++) {
+			int x = (i % boardWidth) * w;
+			int y = (i / boardWidth) * h;
+			Stone space = boardData[i];
+
+			if (space == Stone.BLACK) {
+				g.drawImage(blackStone, x, y, null);
+			} else if (space == Stone.WHITE) {
+				g.drawImage(whiteStone, x, y, null);
+			}
+		}
 	}
 }
 
