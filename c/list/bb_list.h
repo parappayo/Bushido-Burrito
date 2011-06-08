e2b46ecd275cfb236fd590a7c3f8fed2fedee288
Jason Estey <esteyj@yahoo.com>
2011-06-08 09:34:10 -0700

    expanded on the list macros, wrote more test

--- c/list/bb_list.h
+++ c/list/bb_list.h
@@ -10,19 +10,43 @@
 	struct type_t* prev;\
 
 #define BB_LIST_FUNCS(type_t) \
-	void type_t##_init_list(struct type_t* node);\
-	void type_t##_insert_before(struct type_t* list, struct type_t* node);\
-	void type_t##_insert_after(struct type_t* list, struct type_t* node);\
-	void type_t##_add_head(struct type_t* list, struct type_t* node);\
-	void type_t##_add_tail(struct type_t* list, struct type_t* node);\
+	void type_t##_list_init(struct type_t* node);\
+	void type_t##_list_remove(struct type_t* node);\
+	struct type_t* type_t##_list_head(struct type_t* list);\
+	struct type_t* type_t##_list_tail(struct type_t* list);\
+	void type_t##_list_insert_before(struct type_t* list, struct type_t* node);\
+	void type_t##_list_insert_after(struct type_t* list, struct type_t* node);\
+	void type_t##_list_add_head(struct type_t* list, struct type_t* node);\
+	void type_t##_list_add_tail(struct type_t* list, struct type_t* node);\
 \
-void type_t##_init_list(struct type_t* node)\
+void type_t##_list_init(struct type_t* node)\
 {\
 	node->next = NULL;\
 	node->prev = NULL;\
 }\
 \
-void type_t##_insert_before(struct type_t* list, struct type_t* node)\
+void type_t##_list_remove(struct type_t* node)\
+{\
+	if (node->prev) {\
+		node->prev->next = node->next;\
+	}\
+	if (node->next) {\
+		node->next->prev = node->prev;\
+	}\
+}\
+struct type_t* type_t##_list_head(struct type_t* list)\
+{\
+	struct type_t* retval = list;\
+	while (retval->prev) { retval = retval->prev; }\
+	return retval;\
+}\
+struct type_t* type_t##_list_tail(struct type_t* list)\
+{\
+	struct type_t* retval = list;\
+	while (retval->next) { retval = retval->next; }\
+	return retval;\
+}\
+void type_t##_list_insert_before(struct type_t* list, struct type_t* node)\
 {\
 	node->prev = list->prev;\
 	node->next = list;\
@@ -32,7 +56,7 @@ void type_t##_insert_before(struct type_t* list, struct type_t* node)\
 	list->prev = node;\
 }\
 \
-void type_t##_insert_after(struct type_t* list, struct type_t* node)\
+void type_t##_list_insert_after(struct type_t* list, struct type_t* node)\
 {\
 	node->prev = list;\
 	node->next = list->next;\
@@ -42,18 +66,22 @@ void type_t##_insert_after(struct type_t* list, struct type_t* node)\
 	list->next = node;\
 }\
 \
-void type_t##_add_head(struct type_t* list, struct type_t* node)\
+void type_t##_list_add_head(struct type_t* list, struct type_t* node)\
 {\
 	struct type_t* old_head = list;\
 	while (old_head->prev) { old_head = old_head->prev; }\
 	old_head->prev = node;\
+	node->prev = NULL;\
+	node->next = old_head;\
 }\
 \
-void type_t##_add_tail(struct type_t* list, struct type_t* node)\
+void type_t##_list_add_tail(struct type_t* list, struct type_t* node)\
 {\
 	struct type_t* old_tail = list;\
 	while (old_tail->next) { old_tail = old_tail->next; }\
 	old_tail->next = node;\
+	node->prev = old_tail;\
+	node->next = NULL;\
 }\
 
 #endif // __bb_list_h__
--- c/list/test.c
+++ c/list/test.c
@@ -8,28 +8,83 @@ typedef struct node {
 
 	BB_LIST_MEMBERS(node)
 
-	int i;
+	int data;
 } node;
 
 BB_LIST_FUNCS(node)
 
+void print_list(node* list)
+{
+	for (node* i = list; i != NULL; i = i->next) {
+		printf("%d, ", i->data);
+	}
+	printf("\n");
+}
+
 int main(int argc, char** argv)
 {
 	node test_list;
-	test_list.i = -1;
-	node_init_list(&test_list);
+	test_list.data = -1;
+	node_list_init(&test_list);
 
 	for (int i = 0; i < 25; i++) {
 		node* new_node = malloc(sizeof(node));
-		new_node->i = i;
-		node_init_list(new_node);
-		node_add_tail(&test_list, new_node);
+		new_node->data = i;
+		node_list_init(new_node);
+		node_list_add_tail(&test_list, new_node);
 	}
 
-	for (node* i = &test_list; i != NULL; i = i->next) {
-		printf("%d, ", i->i);
+	printf("after populating:\n");
+	print_list(&test_list);
+
+	printf("walking backwards:\n");
+	for (node* i = node_list_tail(&test_list); i->prev != NULL; i = i->prev) {
+		printf("%d, ", i->data);
 	}
 	printf("\n");
+
+	// simple insertion test
+	{
+		node* test_node = &test_list;
+		for (int i = 0; i < 5; i++) {
+			test_node = test_node->next;
+		}
+		node* new_node = malloc(sizeof(node));
+		new_node->data = 99;
+		node_list_insert_after(test_node, new_node);
+	}
+
+	printf("after insert test:\n");
+	print_list(&test_list);
+
+	// simple deletion test
+	{
+		node* test_node = &test_list;
+		for (int i = 0; i < 6; i++) {
+			test_node = test_node->next;
+		}
+		node_list_remove(test_node);
+		free(test_node);
+	}
+
+	printf("after remove test:\n");
+	print_list(&test_list);
+
+	{
+		node* i = node_list_tail(&test_list);
+		printf("list tail: %d\n", i->data);
+		printf("list tail prev: %x\n", (unsigned int) i->prev);
+	}
+
+	for (node* i = node_list_tail(&test_list); i->prev != NULL; ) {
+		node* prev = i->prev;
+		node_list_remove(i);
+		free(i);
+		i = prev;
+	}
+
+	printf("after cleanup:\n");
+	print_list(&test_list);
 
 	return 0;
 }
