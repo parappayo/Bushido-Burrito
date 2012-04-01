d69467661ec1f5f427e9da8b20a4e60761e18a54
Jason Estey <esteyj@yahoo.com>
2012-04-01 10:45:17 -0700

    made some fixes to fox & hounds, still not working

--- prolog/fox_and_hounds.pl
+++ prolog/fox_and_hounds.pl
@@ -11,6 +11,15 @@ not_member(_, []).
 not_member(X, [F | R]) :-
 	not_member(X, R), X \== F.
 
+substitute(_, _, [], []).
+
+substitute(X, Y, [X | RX], [Y | RY]) :-
+	substitute(X, Y, RX, RY).
+
+substitute(X, Y, [Z | RX], [Z | RY]) :-
+	X \= Z,
+	substitute(X, Y, RX, RY).
+
 board_position([X, Y]) :-
 	member(X, [1, 2, 3, 4]),
 	member(Y, [1, 2, 3, 4, 5, 6, 7, 8]).
@@ -41,12 +50,12 @@ board_state_list([First | Rest]) :-
 %
 %  in one variant, the fox's starting position is fixed
 %
-%starting_state([ [1,8], [1,1], [2,1], [3,1], [4,1] ]).
+starting_state([ [1,8], [1,1], [2,1], [3,1], [4,1] ]).
 
-starting_state([ Fox | Hounds ]) :-
-	Hounds = [ [1,1], [2,1], [3,1], [4,1] ],
-	board_position(Fox),
-	not_member(Fox, Hounds).
+%starting_state([ Fox | Hounds ]) :-
+%	Hounds = [ [1,1], [2,1], [3,1], [4,1] ],
+%	board_position(Fox),
+%	not_member(Fox, Hounds).
 
 %
 %  some shared prereqs for valid moves by either player
@@ -58,66 +67,83 @@ valid_move_([OldPos, NewPos, BoardState]) :-
 	OldPos \== NewPos,
 	not_member(NewPos, BoardState).
 
-valid_hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 + 1,
 	X2 =:= X1.
 
-valid_hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 + 1,
 	Y1 mod 2 =:= 0,
 	X2 =:= X1 - 1.
 
-valid_hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+hound_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 + 1,
 	Y1 mod 2 =:= 1,
 	X2 =:= X1 + 1.
 
-valid_hound_move([Pos1, Pos2, [Fox | Hounds]]) :-
-	valid_hound_move_([Pos1, Pos2, [Fox | Hounds]]),
+hound_move([Pos1, Pos2, [Fox | Hounds]]) :-
+	hound_move_([Pos1, Pos2, [Fox | Hounds]]),
 	member(Pos1, Hounds).
 
-valid_fox_move_(M) :-
-	valid_hound_move_(M).
+fox_move_(M) :-
+	hound_move_(M).
 
-valid_fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 - 1,
 	X2 =:= X1.
 
-valid_fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 - 1,
 	Y1 mod 2 =:= 0,
 	X2 =:= X1 + 1.
 
-valid_fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
+fox_move_([[X1, Y1], [X2, Y2], BoardState]) :-
 	valid_move_([[X1, Y1], [X2, Y2], BoardState]),
 	Y2 =:= Y1 - 1,
 	Y1 mod 2 =:= 1,
 	X2 =:= X1 - 1.
 
-valid_fox_move([Pos1, Pos2, [Fox | Hounds]]) :-
+fox_move([Pos1, Pos2, [Fox | Hounds]]) :-
 	board_state([Fox | Hounds]),
 	Pos1 = Fox,
-	valid_fox_move_([Pos1, Pos2, [Fox | Hounds]]).
+	fox_move_([Pos1, Pos2, [Fox | Hounds]]).
 
 no_hounds_moves_left(BoardState) :-
-	valid_hound_move([_, _, BoardState]), !, fail ; true.
+	hound_move([_, _, BoardState]), !, fail ; true.
 
 no_fox_moves_left(BoardState) :-
-	valid_fox_move([_, _, BoardState]), !, fail ; true.
+	fox_move([_, _, BoardState]), !, fail ; true.
 
-game_sequence([]).
+linked_moves([]).
 
-game_sequence([ FirstMove, SecondMove | Rest ]) :-
-	FirstMove = [ _, _, BoardState ],
+linked_moves([ [P1, P2, State1], [_, _, State2] | Rest ]) :-
+	substitute(P1, P2, State1, State2),
+	linked_moves(Rest).
+
+alternating_moves([]).
+
+% can end on a hound move if the fox has no moves left
+alternating_moves([ Move ]) :-
+	hound_move(Move),
+	Move = [_, _, BoardState],
+	no_fox_moves_left(BoardState).
+
+alternating_moves([ First, Second | Rest ]) :-
+	hound_move(First),
+	fox_move(Second),
+	linked_moves([ First, Second ]),
+	alternating_moves(Rest).
+
+game_sequence(S) :-
+	S = [ [_, _, BoardState], _ | _ ],
 	starting_state(BoardState),
-	valid_hound_move(FirstMove),
-	valid_fox_move(SecondMove),
-	game_sequence(Rest).
+	alternating_moves(S),
+	linked_moves(S).
 
 hounds_victory(S) :-
 	game_sequence(S),
