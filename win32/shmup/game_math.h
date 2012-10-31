2a75ec09417ac6f371f354d017e872a9e4173dac
Jason Estey <esteyj@yahoo.com>
2012-10-31 15:01:14 -0700

    resolving conflicts from the last push

--- win32/shmup/game_math.h
+++ win32/shmup/game_math.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #ifndef GAME_MATH_H
 #define GAME_MATH_H
@@ -11,17 +10,4 @@ float game_sin(float x);
 float game_cos(float x);
 
 #endif // GAME_MATH_H
-=======
 
-#ifndef GAME_MATH_H
-#define GAME_MATH_H
-#pragma once
-
-#define MATH_PI 3.1415927;
-
-float game_sqrt(float x);
-float game_sin(float x);
-float game_cos(float x);
-
-#endif // GAME_MATH_H
->>>>>>> 64804cb5ab8e44f688f08e71e1c8fc781727c8dc
