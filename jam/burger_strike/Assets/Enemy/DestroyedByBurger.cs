49e723507173248401a112d059b0e227df4f1bba
Jason Estey <esteyj@yahoo.com>
2015-04-18 21:30:29 -0700

    proper burger destruction logic

---
+++ jam/burger_strike/Assets/Enemy/DestroyedByBurger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyedByBurger : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Burger"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
