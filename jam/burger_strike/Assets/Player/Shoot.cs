2102f04cdc8123a6f16282562c2ee31d575a0765
Jason Estey <esteyj@yahoo.com>
2015-04-18 20:52:57 -0700

    can shoot with spacebar or joystick

---
+++ jam/burger_strike/Assets/Player/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject Explosion;
+	public float ShootRefreshTime = 0.2f;
+
+	private float _ShootElapsed = 0.0f;
+
+	void Update() {
+
+		if (_ShootElapsed < ShootRefreshTime)
+		{
+			_ShootElapsed += Time.deltaTime;
+			return;
+		}
+	
+		if (Input.GetButtonDown("Fire1") ||
+		    Input.GetButtonDown("Jump"))
+		{
+			TakeShot();
+		}
+	}
+
+	void TakeShot() {
+
+		_ShootElapsed = 0.0f;
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.up, out hit, 200.0f))
+		{
+			Instantiate(Explosion, hit.point, Quaternion.identity);
+		}
+	}
+}
