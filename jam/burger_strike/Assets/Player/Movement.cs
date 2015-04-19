6960246736b813195a0b4707dc23f7294945caf3
Jason Estey <esteyj@yahoo.com>
2015-04-18 17:44:06 -0700

    added physics and some crude heli movement

--- jam/burger_strike/Assets/Player/Movement.cs
+++ jam/burger_strike/Assets/Player/Movement.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	public float ForwardSpeed = 20.0f;
-	public float LateralSpeed = 10.0f;
+	public float ForwardThrust = 20.0f;
+	public float LateralThrust = 10.0f;
+	public float RotationThrust = 5.0f;
 
-	void Start () {
+	private Rigidbody _RigidBody;
+	private Vector3 _InitialForward;
+	private Vector3 _InitialUp;
+
+	void Start() {
 	
+		_RigidBody = GetComponent<Rigidbody>();
+		_InitialForward = transform.forward;
+		_InitialUp = transform.up;
 	}
-	
-	void Update () {
 
-		float forward = Input.GetAxis("Vertical") * ForwardSpeed * Time.deltaTime;
-		float lateral = Input.GetAxis("Horizontal") * LateralSpeed * Time.deltaTime;
-		Vector3 move = new Vector3(lateral, 0, forward);
-		transform.position += move;
+	void Update() {
+
+		float timeFactor = Time.deltaTime * 100.0f;
+		float forward = Input.GetAxis("Vertical") * ForwardThrust * timeFactor;
+		float lateral = Input.GetAxis("Horizontal") * LateralThrust * timeFactor;
+		float rotation = Input.GetAxis("Horizontal") * RotationThrust * timeFactor;
+
+		Vector3 thrust = new Vector3(lateral, 0, forward);
+		_RigidBody.AddForce(thrust);
+
+		Vector3 torque = new Vector3(0, rotation, 0);
+		_RigidBody.AddTorque(torque);
+
+		// maintain upright orientation
+		_RigidBody.rotation = Quaternion.Slerp(
+			_RigidBody.rotation,
+			Quaternion.LookRotation(_InitialForward, _InitialUp),
+			Time.deltaTime);
 	}
 }
---
+++ jam/burger_strike/readme.txt
@@ -0,0 +1,24 @@
+
+Burger Strike
+
+A demo / game for Ludum Dare 32 (theme: An Unconventional Weapon)
+
+
+Unity Asset Store Links
+
+Low Polygon Sci-Fi Helicopter
+by Brian Stärke
+https://www.assetstore.unity3d.com/en/#!/content/25725
+
+Sci Fi Army / Ground units 02
+by CGPitbull
+https://www.assetstore.unity3d.com/en/#!/content/14565
+
+Deatrocker's food pack #1
+bty Deatrocker
+https://www.assetstore.unity3d.com/en/#!/content/29958
+
+Alpine Terrain Pack
+by Quantum Theory
+https://www.assetstore.unity3d.com/en/#!/content/2933
+
