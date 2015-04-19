e2c34e76fe703377645595a004e9b265b851842a
Jason Estey <esteyj@yahoo.com>
2015-04-18 18:21:09 -0700

    added explosions

---
+++ jam/burger_strike/Assets/Explosion/DespawnTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DespawnTimer : MonoBehaviour {
+
+	public float Lifetime = 5.0f;
+	private float _Elapsed = 0.0f;
+
+	void Update() {
+	
+		_Elapsed += Time.deltaTime;
+		if (_Elapsed > Lifetime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ jam/burger_strike/Assets/Explosion/ExplodeOnTouch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnTouch : MonoBehaviour {
+
+	public GameObject Explosion;
+
+	private Rigidbody _Rigidbody;
+
+	void Start() {
+
+		_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+	
+		Vector2 pos = new Vector2();
+		if (GetClickPosition(out pos))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(pos), out hit))
+			{
+				if (hit.rigidbody == _Rigidbody)
+				{
+					Instantiate(Explosion, hit.point, Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	bool GetClickPosition(out Vector2 pos)
+	{
+		if (Input.touchCount > 0)
+		{
+			pos = Input.touches[0].position;
+			return true;
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			pos = Input.mousePosition;
+			return true;
+		}
+
+		pos = Vector2.zero;
+		return false;
+	}
+}
