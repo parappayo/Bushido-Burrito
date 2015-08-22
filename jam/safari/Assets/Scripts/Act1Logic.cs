921bb8af3f422f19cd4cf72170a51c3e7528d6c3
Jason Estey <esteyj@yahoo.com>
2015-08-22 11:04:45 -0700

    starting to stub in the two act structure

---
+++ jam/safari/Assets/Scripts/Act1Logic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Act1Logic : MonoBehaviour {
+
+	public GameObject Act2;
+
+	void OnEnable()
+	{
+		if (Act2 != null)
+		{
+			Act2.SetActive(false);
+		}
+	}
+
+	void OnDisable()
+	{
+		if (Act2 != null)
+		{
+			Act2.SetActive(true);
+		}
+	}
+}
---
+++ jam/safari/Assets/Scripts/Act2Logic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Act2Logic : MonoBehaviour {
+
+	public GameObject Act1;
+
+	void OnEnable()
+	{
+		if (Act1 != null)
+		{
+			Act1.SetActive(false);
+		}
+	}
+	
+	void OnDisable()
+	{
+		if (Act1 != null)
+		{
+			Act1.SetActive(true);
+		}
+	}
+}
