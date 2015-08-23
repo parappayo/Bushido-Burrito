1d1e173a5b3e9b43079366030d1e41e9aa3f8959
Jason Estey <esteyj@yahoo.com>
2015-08-23 12:44:15 -0700

    improved captions and act transition

--- jam/safari/Assets/Scripts/Act1Logic.cs
+++ jam/safari/Assets/Scripts/Act1Logic.cs
@@ -6,6 +6,8 @@ public class Act1Logic : MonoBehaviour {
 	public GameObject Act2;
 	public Player ThePlayer;
 	public Lion TheLion;
+	public float IntroCaptionTimeout = 3.5f;
+	public float OutroCaptionTimeout = 3.5f;
 	public GUIStyle TextStyle;
 
 	private Flow _Flow;
@@ -52,7 +54,7 @@ public class Act1Logic : MonoBehaviour {
 		switch (_Flow.State)
 		{
 			case Flow.eState.INTRO:
-				if (_Flow.StateTimer >= 3f)
+				if (_Flow.StateTimer >= IntroCaptionTimeout)
 				{
 					_Flow.State = Flow.eState.NORMAL;
 				}
@@ -67,11 +69,25 @@ public class Act1Logic : MonoBehaviour {
 				break;
 
 			case Flow.eState.OUTRO:
-				if (_Flow.StateTimer >= 3f)
+				if (_Flow.StateTimer >= OutroCaptionTimeout)
 				{
 					_Flow.State = Flow.eState.IDLE;
 				}
 				break;
+		}
+	}
+
+	private float CaptionAlpha
+	{
+		get
+		{
+			return TextStyle.normal.textColor.a;
+		}
+		set
+		{
+			Color c = TextStyle.normal.textColor;
+			c.a = value;
+			TextStyle.normal.textColor = c;
 		}
 	}
 
@@ -80,6 +96,7 @@ public class Act1Logic : MonoBehaviour {
 		switch (_Flow.State)
 		{
 			case Flow.eState.INTRO:
+				CaptionAlpha = Mathf.Clamp01(IntroCaptionTimeout * (IntroCaptionTimeout - _Flow.StateTimer) / IntroCaptionTimeout);
 				GUI.Label(
 					new Rect(Screen.width * 0.2f, Screen.height * 0.3f, Screen.width * 0.6f, Screen.height * 0.3f),
 					"SAFARI",
@@ -87,9 +104,10 @@ public class Act1Logic : MonoBehaviour {
 				break;
 
 			case Flow.eState.OUTRO:
+				CaptionAlpha = Mathf.Clamp01(OutroCaptionTimeout * (OutroCaptionTimeout - _Flow.StateTimer) / OutroCaptionTimeout);
 				GUI.Label(
 					new Rect(Screen.width * 0.2f, Screen.height * 0.3f, Screen.width * 0.6f, Screen.height * 0.3f),
-					"BEGIN ACT 2",
+					"NICE KILL",
 					TextStyle);
 				break;
 		}
--- jam/safari/Assets/Scripts/Act2Logic.cs
+++ jam/safari/Assets/Scripts/Act2Logic.cs
@@ -5,9 +5,19 @@ public class Act2Logic : MonoBehaviour {
 
 	public GameObject Act1;
 	public Player ThePlayer;
+	public float IntroCaptionTimeout = 3.5f;
+	public float OutroCaptionTimeout = 3.5f;
+	public GUIStyle TextStyle;
+
+	private Flow _Flow;
 
 	void OnEnable()
 	{
+		_Flow = new Flow();
+		_Flow.OnEnterState += new StateChangedHandler(OnEnterState);
+		_Flow.OnExitState += new StateChangedHandler(OnExitState);
+		_Flow.State = Flow.eState.INTRO;
+
 		if (Act1 != null)
 		{
 			Act1.SetActive(false);
@@ -21,9 +31,78 @@ public class Act2Logic : MonoBehaviour {
 	
 	void OnDisable()
 	{
+		_Flow.State = Flow.eState.IDLE;
+		_Flow.OnEnterState -= new StateChangedHandler(OnEnterState);
+		_Flow.OnExitState -= new StateChangedHandler(OnExitState);
+
 		if (Act1 != null)
 		{
 			Act1.SetActive(true);
 		}
+	}
+
+	void Update()
+	{
+		_Flow.Update(Time.deltaTime);
+
+		switch (_Flow.State)
+		{
+			case Flow.eState.INTRO:
+				if (_Flow.StateTimer >= IntroCaptionTimeout)
+				{
+					_Flow.State = Flow.eState.NORMAL;
+				}
+				break;
+
+			case Flow.eState.OUTRO:
+				if (_Flow.StateTimer >= OutroCaptionTimeout)
+				{
+					_Flow.State = Flow.eState.IDLE;
+				}
+				break;
+		}
+	}
+
+	private float CaptionAlpha
+	{
+		get
+		{
+			return TextStyle.normal.textColor.a;
+		}
+		set
+		{
+			Color c = TextStyle.normal.textColor;
+			c.a = value;
+			TextStyle.normal.textColor = c;
+		}
+	}
+
+	void OnGUI()
+	{
+		switch (_Flow.State)
+		{
+			case Flow.eState.INTRO:
+				CaptionAlpha = Mathf.Clamp01(IntroCaptionTimeout * (IntroCaptionTimeout - _Flow.StateTimer) / IntroCaptionTimeout);
+				GUI.Label(
+					new Rect(Screen.width * 0.2f, Screen.height * 0.3f, Screen.width * 0.6f, Screen.height * 0.3f),
+					"AT THE OFFICE",
+					TextStyle);
+				break;
+		}
+	}
+
+	private void OnEnterState(Flow flow, Flow.eState oldState, Flow.eState newState)
+	{
+		switch (newState)
+		{
+			case Flow.eState.IDLE:
+				gameObject.SetActive(false);
+				break;
+		}
+	}
+
+	private void OnExitState(Flow flow, Flow.eState oldState, Flow.eState newState)
+	{
+
 	}
 }
