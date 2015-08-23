20e0a3e11eb080dc6fcad591aa1c175b89e5d8af
Jason Estey <esteyj@yahoo.com>
2015-08-22 23:43:58 -0700

    lion leg animation

---
+++ jam/safari/Assets/Lion/PlayerShotTargetHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShotTargetHelper : MonoBehaviour {
+
+	public Lion TheLion;
+
+	public void OnShotByPlayer(RaycastHit hit)
+	{
+		if (TheLion != null)
+		{
+			TheLion.OnShotByPlayer(hit);
+		}
+	}
+}
---
+++ jam/safari/Assets/Lion/SwingAnim.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingAnim : MonoBehaviour {
+
+	public Vector3 RotationAxis;
+	public float RotationSpeed = 1f;
+	public float RotationOffset = 0f;
+	public float TimeOffset = 0f;
+
+	void Update()
+	{
+		float rotationDegrees = RotationOffset + Mathf.Sin((Time.time + TimeOffset) * RotationSpeed) * (180f / Mathf.PI);
+		transform.localRotation = Quaternion.AngleAxis(rotationDegrees, RotationAxis);
+	}
+}
