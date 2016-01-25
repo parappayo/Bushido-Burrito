04bcee0b394382d9ade5268b8ee3156a0e243383
Jason Estey <esteyj@yahoo.com>
2016-01-24 20:50:56 -0800

    added sway motion

---
+++ skunk/unity/danmaku/Assets/Sway.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class Sway : MonoBehaviour
+{
+	public Vector3 Magnitude = Vector3.zero;
+	public float Speed = 1f;
+
+	private Vector3 PreviousOffset = Vector3.zero;
+	private float LifeTimer = 0f;
+
+	private void Update()
+	{
+		LifeTimer += Time.deltaTime;
+
+		Vector3 offset = Magnitude * (Mathf.Sin(LifeTimer) * Speed);
+		transform.position = transform.position + (offset - PreviousOffset);
+		PreviousOffset = offset;
+	}
+}
