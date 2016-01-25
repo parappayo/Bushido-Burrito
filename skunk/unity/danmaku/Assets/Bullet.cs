b5236b715858b5cf6b4e474038db5fc988627563
Jason Estey <esteyj@yahoo.com>
2016-01-24 19:36:38 -0800

    danmaku sandbox stub project

---
+++ skunk/unity/danmaku/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public Vector3 Velocity;
+	public float Lifetime = 5f;
+
+	private float LifeTimer = 0f;
+
+	private void Update()
+	{
+		LifeTimer += Time.deltaTime;
+		if (LifeTimer > Lifetime)
+		{
+			Despawn();
+		}
+
+		transform.position += Velocity * Time.deltaTime;
+	}
+
+	private void Despawn()
+	{
+		// TODO: pool bullets
+		Destroy(gameObject);
+	}
+}
---
+++ skunk/unity/danmaku/Assets/Emitter.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+	public GameObject Spawnable;
+	public float SpawnPeriod = 0.5f;
+	public bool UseAsSpawnParent = true;
+
+	public delegate Vector3 VelocityDelegate(float t);
+	public VelocityDelegate VelocityFunction;
+
+	private float SpawnTimer = 0f;
+	private float EmitterAge = 0f;
+
+	private Vector3 SpiralVelocity(float t)
+	{
+		return new Vector3(Mathf.Sin(t), Mathf.Cos(t), 0f);
+	}
+
+	private void Start()
+	{
+		VelocityFunction = SpiralVelocity;
+	}
+
+	private void Update()
+	{
+		EmitterAge += Time.deltaTime;
+
+		SpawnTimer -= Time.deltaTime;
+		if (SpawnTimer <= 0f)
+		{
+			Spawn();
+			SpawnTimer = SpawnPeriod;
+		}
+	}
+
+	public void Spawn()
+	{
+		if (Spawnable == null || VelocityFunction == null)
+		{
+			return;
+		}
+
+		// TODO: pool bullets
+		GameObject spawnObject = Instantiate(Spawnable);
+		if (UseAsSpawnParent)
+		{
+			spawnObject.transform.parent = transform;
+		}
+
+		Bullet bullet = spawnObject.GetComponent<Bullet>();
+		bullet.Velocity = VelocityFunction(EmitterAge);
+	}
+}
