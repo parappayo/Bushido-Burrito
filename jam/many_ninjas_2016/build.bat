a1707062a8a8f081b409d73b358220b4fbdd492f
Jason Estey <esteyj@yahoo.com>
2016-02-21 15:46:54 -0800

    created sprite atlas sheet, fixed build batch file

--- jam/many_ninjas_2016/build.bat
+++ jam/many_ninjas_2016/build.bat
@@ -1,2 +1,14 @@
+
+set BUILD_DIR=build
+mkdir %BUILD_DIR%
+
+REM  Code Build
 call tsc
-copy src\index.html build
+copy src\index.html %BUILD_DIR%
+mkdir %BUILD_DIR%\lib
+copy lib\*.js %BUILD_DIR%\lib
+
+REM  Art Assets
+mkdir %BUILD_DIR%\atlases
+copy assets\atlases\*.png %BUILD_DIR%\atlases
+copy assets\atlases\*.json %BUILD_DIR%\atlases
