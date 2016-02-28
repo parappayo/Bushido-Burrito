b7fc838e9d100f1fc514ecb8156e3efb84bb34a7
Jason Estey <esteyj@yahoo.com>
2016-02-27 20:06:35 -0800

    cleaned up Menu system a bit

--- jam/many_ninjas_2016/src/ninjas_console.ts
+++ jam/many_ninjas_2016/src/ninjas_console.ts
@@ -3,48 +3,78 @@
 
 import readline = require("readline");
 
-var menuOptions = {
-	"t" : {
-		caption: "(t)ick sim",
-		handler: tickSim 
+interface MenuCommandHandler
+{
+	() : void;
+}
+
+interface MenuOption
+{
+	caption : string,
+	handler : MenuCommandHandler
+}
+
+class Menu
+{
+	public options = {};
+
+	print()
+	{
+		for (var key in this.options) {
+			var menuOption :MenuOption = this.options[key];
+			console.log(menuOption.caption);
+		}
 	}
-};
+
+	hasCommand(command :string) :boolean
+	{
+		return command in this.options;
+	}
+
+	handleCommand(command : string)
+	{
+		this.options[command].handler();
+	}
+}
 
 function tickSim()
 {
 	console.log("should tick sim here");
 }
 
-function printMenu(menuOptions)
-{
-	for (var key in menuOptions) {
-		var option = menuOptions[key];
-		console.log(option.caption);
-	}
-}
-
 function main()
 {
+	var menu : Menu = new Menu();
+	menu.options = {
+		"t" : {
+			caption: "(t)ick sim",
+			handler: tickSim 
+		},
+		"q" : {
+			caption: "(q)uit",
+			handler: () => { process.exit(0); }
+		}
+	};
+
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout
 	});
 
 	rl.setPrompt(">");
-	printMenu(menuOptions);
+	menu.print();
 	rl.prompt();
 
 	rl.on("line", line => {
 		var command : string = line.trim();
-		if (command in menuOptions)
-		{
-			menuOptions[command].handler();
-		}
-		else
-		{
+		if (command.length < 1) {
+			// do nothing, just re-prompt
+		} else if (menu.hasCommand(command)) {
+			menu.handleCommand(command);
+		} else {
 			console.log("unrecognized command: " + command);
 		}
-		printMenu(menuOptions);
+		menu.print();
 		rl.prompt();
 	}).on("close", () => {
 		process.exit(0);
