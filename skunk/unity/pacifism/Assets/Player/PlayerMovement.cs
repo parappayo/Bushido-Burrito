26d1b0633eb6d166bcd8c2cbd3a12a6f05b6bec0
Jason Estey <esteyj@yahoo.com>
2016-07-08 19:54:36 -0700

    added pacifism demo project (twin stick shmup)

---
+++ skunk/unity/pacifism/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    void Update()
+    {
+        transform.position = transform.position + (transform.rotation * Movement);
+    }
+
+    private Vector3 Movement
+    {
+        get
+        {
+            var scale = Speed * Time.deltaTime;
+            return new Vector3(
+                Input.GetAxis("Horizontal") * scale,
+                Input.GetAxis("Vertical") * scale,
+                0);
+        }
+    }
+
+} // class
---
+++ skunk/unity/pacifism/Assets/Seeker/Follow.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    public GameObject Target;
+
+    void Update()
+    {
+        transform.position = transform.position + (transform.rotation * Movement);
+    }
+
+    private Vector3 Movement
+    {
+        get
+        {
+            if (!Target) { return Vector3.zero;  }
+            var scale = Speed * Time.deltaTime;
+            var direction = Target.transform.position - transform.position;
+            return direction.normalized * scale;
+        }
+    }
+
+} // class
