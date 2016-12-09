5d501bfcc6463fcfdae6a7afe760069feef62e43
Jason Estey <esteyj@yahoo.com>
2016-12-09 13:22:47 -0800

    simple spawner

---
+++ skunk/unity/pacifism/Assets/Spawner/Spawner.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject Spawnable;
+	public bool UseAsSpawnParent = true;
+
+	public float TriggerTimeScale = 1f;
+
+	public delegate bool TriggerDelegate(float t);
+	public TriggerDelegate ShouldSpawn;
+
+	private void FixedUpdate()
+	{
+		if (ShouldSpawn == null) { return; }
+
+		if (ShouldSpawn(Time.deltaTime * TriggerTimeScale))
+		{
+			Spawn();
+		}
+	}
+
+	public void Spawn()
+	{
+		if (Spawnable == null) { return; }
+
+		GameObject spawnObject = Instantiate(Spawnable);
+		spawnObject.transform.position = transform.position;
+		if (UseAsSpawnParent)
+		{
+			spawnObject.transform.parent = transform;
+		}
+	}
+}
