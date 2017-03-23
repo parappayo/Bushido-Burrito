bd0123662a1006168ece1d476c9d1ea751ddc22e
Jason Estey <esteyj@yahoo.com>
2017-03-23 16:22:05 -0700

    focus already open node edit form if it exists

--- skunk/cs/graph_edit/UI/GraphEditForm.cs
+++ skunk/cs/graph_edit/UI/GraphEditForm.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Windows.Forms;
+using GraphEdit.Graph;
 
 namespace GraphEdit.UI
 {
@@ -15,10 +16,36 @@ namespace GraphEdit.UI
             this.nodeListControl.ItemOpened += this.HandleNodeOpened;
         }
 
+        private NodeEditForm FindNodeEditForm(Node node)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                var nodeEditForm = form as NodeEditForm;
+
+                if (nodeEditForm != null &&
+                    nodeEditForm.Target == node)
+                {
+                    return nodeEditForm;
+                }
+            }
+
+            return null;
+        }
+
         private void HandleNodeOpened(object sender, EventArgs e)
         {
-            NodeEditForm nodeEditForm = new NodeEditForm(this.nodeListControl.SelectedItem);
-            nodeEditForm.Show();
+            var selectedNode = this.nodeListControl.SelectedItem;
+            var nodeEditForm = FindNodeEditForm(selectedNode);
+
+            if (nodeEditForm != null)
+            {
+                nodeEditForm.Focus();
+            }
+            else
+            {
+                nodeEditForm = new NodeEditForm(selectedNode);
+                nodeEditForm.Show();
+            }
         }
 
         private void addNodeButton_Click(object sender, EventArgs e)
