b668292f6863b6fe46542a531385916e202aa410
Jason Estey <esteyj@yahoo.com>
2017-04-26 16:18:59 -0700

    added ability to offset curve, some refactoring

--- skunk/js/curves/curves.js
+++ skunk/js/curves/curves.js
@@ -23,7 +23,15 @@ Curve.prototype.scale = function (scale)
 	});
 }
 
-Curve.prototype.plot = function (canvasContext2D, points)
+Curve.prototype.translate = function (x, y)
+{
+	this.points.forEach((currentValue, index, array) => {
+		currentValue.x += x;
+		currentValue.y += y;
+	});
+}
+
+Curve.prototype.plot = function (canvasContext2D)
 {
 	if (this.points.length < 2) { return; }
 
@@ -49,59 +57,38 @@ ParametricFunction.prototype.generateCurve = function (range, stepCount)
 
 	var t = range.start;
 	var dt = range.delta() / stepCount;
-
-	while (stepCount > 0) {
-		stepCount--;
-
-		t += dt;
-		var point = {
-			x: this.x(t),
-			y: this.y(t)
-		};
-
-		curve.points.push(point);
-	}
-
-	return curve;
-}
-
-
-ParametricFunction.prototype.generateCumulativeCurve = function (range, stepCount)
-{
-	var curve = new Curve();
-
-	var t = range.start;
-	var dt = range.delta() / stepCount;
 	var previousPoint = { x: this.x(t), y: this.y(t) };
 
-	while (stepCount > 0) {
-		stepCount--;
+	if (this.isCumulativeCurve) {
+		while (stepCount > 0) {
+			stepCount--;
 
-		t += dt;
-		var point = {
-			x: previousPoint.x + this.x(t),
-			y: previousPoint.y + this.y(t)
-		};
+			t += dt;
+			var point = {
+				x: previousPoint.x + this.x(t),
+				y: previousPoint.y + this.y(t)
+			};
 
-		curve.points.push(point);
-		previousPoint = point;
+			curve.points.push(point);
+			previousPoint = point;
+		}
+
+	} else {
+		while (stepCount > 0) {
+			stepCount--;
+
+			t += dt;
+			var point = {
+				x: this.x(t),
+				y: this.y(t)
+			};
+
+			curve.points.push(point);
+		}
 	}
 
+
 	return curve;
-}
-
-ParametricFunction.prototype.plot = function (canvasContext2D, range, stepCount, scale)
-{
-	var curve = this.generateCurve(range, stepCount);
-	curve.scale(scale);
-	curve.plot(canvasContext2D, curve);
-}
-
-ParametricFunction.prototype.plotCumulative = function (canvasContext2D, range, stepCount, scale)
-{
-	var curve = this.generateCumulativeCurve(range, stepCount);
-	curve.scale(scale);
-	curve.plot(canvasContext2D, curve);
 }
 
 ParametricFunction.Spiral = function ()
@@ -117,10 +104,14 @@ ParametricFunction.EulerSpiral = function (range, stepCount)
 {
 	var dt = range.delta() / stepCount;
 
-	return new ParametricFunction(
+	var func = new ParametricFunction(
 			(t) => { return Math.cos(t * t) * dt },
 			(t) => { return Math.sin(t * t) * dt }
 		);
+
+	func.isCumulativeCurve = true;
+
+	return func;
 }
 
 // https://en.wikipedia.org/wiki/Butterfly_curve_(transcendental)
@@ -130,4 +121,22 @@ ParametricFunction.ButterflyCurve = function ()
 		(t) => { return Math.sin(t) * (Math.exp(Math.cos(t)) - 2 * Math.cos(4 * t) - Math.pow(Math.sin(t / 12), 5)); },
 		(t) => { return Math.cos(t) * (Math.exp(Math.cos(t)) - 2 * Math.cos(4 * t) - Math.pow(Math.sin(t / 12), 5)); }
 	);
+}
+
+function plotCurve(canvasContext2D, params)
+{
+	if (!params.parametricFunc) {
+		return;
+	}
+
+	var curve = params.parametricFunc.generateCurve(params.range, params.stepCount);
+
+	if (params.scale) {
+		curve.scale(params.scale);
+	}
+	if (params.offset) {
+		curve.translate(params.offset.x, params.offset.y);
+	}
+
+	curve.plot(canvasContext2D);
 }
