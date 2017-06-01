d1bd471e10ef7585d3c46604ffde9ddf39ec0fc2
Jason Estey <esteyj@yahoo.com>
2017-06-01 15:08:36 -0700

    simple fix for sprite z-order

---
+++ skunk/unity/pacifism/Assets/SpriteSetZtoY.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class SpriteSetZtoY : MonoBehaviour
+{
+	public float Scale = 0.0001f;
+
+	private void Update ()
+	{
+		transform.position = new Vector3(
+			transform.position.x,
+			transform.position.y,
+			transform.position.y * Scale);
+	}
+}
