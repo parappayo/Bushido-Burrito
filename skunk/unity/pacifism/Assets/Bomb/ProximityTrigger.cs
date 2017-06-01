4bc16b587093a251faec79a636c015ca4765b4c5
Jason Estey <esteyj@yahoo.com>
2017-06-01 14:04:48 -0700

    added bomb prefab

---
+++ skunk/unity/pacifism/Assets/Bomb/ProximityTrigger.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProximityTrigger : MonoBehaviour
+{
+	public GameObject Target;
+
+	public float ProximityThreshold = 1.0f;
+
+	public UnityEvent Callback;
+
+	public bool Triggered = false;
+
+	public void Update()
+	{
+		if (Triggered || Target == null) { return; }
+
+		if (DistanceToTarget < ProximityThreshold)
+		{
+			Triggered = true;
+
+			if (Callback != null) { Callback.Invoke(); }
+		}
+	}
+
+	public float DistanceToTarget
+	{
+		get
+		{
+			return Math.Abs(Vector3.Distance(Target.transform.position, transform.position));
+		}
+	}
+}
