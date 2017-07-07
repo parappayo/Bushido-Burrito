f8ee123f6fad3d35085272ebaf081b697dd53661
Jason Estey <esteyj@yahoo.com>
2017-07-07 13:15:42 -0700

    added simple reporting of the access log

--- skunk/php/request_log/request_log.php
+++ skunk/php/request_log/request_log.php
@@ -11,22 +11,48 @@ $simple_log_db_table = 'simple_log';
 // function_exists('inet_pton') or die('PHP version does not support inet_pton');
 
 function log_request_simple($db, $table_name, $uri, $date_str, $remote_ip, $user_agent) {
+	$table_name = mysql_real_escape_string($table_name, $db);
 	$uri = mysql_real_escape_string($uri, $db);
 	$date_str = mysql_real_escape_string($date_str);
 	$remote_ip = ip2long($remote_ip);
 	$user_agent = mysql_real_escape_string($user_agent);
 
-	if (!$uri or !$date_str or !$remote_ip or !$user_agent) {
+	if (!$table_name or !$uri or !$date_str or !$remote_ip or !$user_agent) {
 		return FALSE;
 	}
 
 	return mysql_query(sprintf(
-		"INSERT INTO %s (uri, date, remote_ip, user_agent) VALUES('%s', '%s', '%s', '%s')",
-		$table_name,
-		$uri,
-		$date_str,
-		$remote_ip,
-		$user_agent));
+			"INSERT INTO %s (uri, date, remote_ip, user_agent) VALUES('%s', '%s', '%s', '%s')",
+			$table_name,
+			$uri,
+			$date_str,
+			$remote_ip,
+			$user_agent),
+		$db);
+}
+
+function query_log_simple($db, $table_name, $limit=20) {
+	$table_name = mysql_real_escape_string($table_name, $db);
+	$limit = mysql_real_escape_string($limit, $db);
+
+	return mysql_query(sprintf(
+			"SELECT uri, date, remote_ip, user_agent FROM %s ORDER BY date DESC LIMIT %s",
+			$table_name,
+			$limit),
+		$db);
+}
+
+function print_html_log_simple($query_result) {
+	echo('<table>');
+	while ($row = mysql_fetch_array($query_result)) {
+		echo(sprintf(
+			'<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>',
+			$row['uri'],
+			$row['date'],
+			long2ip($row['remote_ip']),
+			$row['user_agent']));
+	}
+	echo('</table>');
 }
 
 $db = mysql_connect($mysql_server, $mysql_user, $mysql_password)
@@ -42,7 +68,12 @@ log_request_simple(
 	date('Y-m-d H:i:s'),
 	$_SERVER['REMOTE_ADDR'],
 	$_SERVER['HTTP_USER_AGENT'])
-	or die('error: failed to log request' . mysql_error());
+	or die('error: failed to log request: ' . mysql_error());
+
+$query_result = query_log_simple($db, $simple_log_db_table)
+	or die('error: failed to query log: ' . mysql_error());
+
+print_html_log_simple($query_result);
 
 mysql_close($db);
 
