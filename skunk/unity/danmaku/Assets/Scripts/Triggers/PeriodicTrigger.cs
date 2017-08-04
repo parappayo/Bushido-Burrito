11fca5be16f4b0da8ffa2899cae4593e47816d5a
Jason Estey <esteyj@yahoo.com>
2017-08-04 10:33:55 -0700

    refactored PeriodicTrigger

--- skunk/unity/danmaku/Assets/Scripts/Triggers/PeriodicTrigger.cs
+++ skunk/unity/danmaku/Assets/Scripts/Triggers/PeriodicTrigger.cs
@@ -1,28 +1,32 @@
 ﻿
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PeriodicTrigger : MonoBehaviour
 {
+	public UnityEvent OnTrigger;
+
+	public float TimeScale = 1.0f;
 	public float Period = 0.5f;
 
 	private float Timer = 0f;
 
 	private bool Trigger(float t)
 	{
-		Timer -= t;
+		this.Timer -= t;
 
-		if (Timer <= 0f)
-		{
-			Timer = Period;
-			return true;
+		if (this.Timer > 0.0f) {
+			return false;
 		}
 
-		return false;
+		this.Timer = this.Period;
+		return true;
 	}
 
-	private void Start()
+	private void FixedUpdate()
 	{
-		Emitter emitter = GetComponent<Emitter>();
-		emitter.TriggerFunction = Trigger;
+		if (Trigger(Time.deltaTime * this.TimeScale)) {
+			this.OnTrigger.Invoke();
+		}
 	}
 }
