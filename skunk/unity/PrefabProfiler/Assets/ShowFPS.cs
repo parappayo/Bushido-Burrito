74c3ffd3cdb9832b3f05fe8dfbc70edf668aebfa
Jason Estey <esteyj@yahoo.com>
2018-01-16 23:31:10 -0800

    added test scene and simple FPS counter

---
+++ skunk/unity/PrefabProfiler/Assets/ShowFPS.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour
+{
+    public bool ShowGUI = true;
+    public GUIStyle Style = new GUIStyle();
+
+    private float deltaTime = 0.0f;
+    private float fps = 0.0f;
+
+    private void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        fps = 1.0f / deltaTime;
+    }
+
+    private void OnGUI()
+    {
+        if (!ShowGUI) { return; }
+
+        Rect rect = new Rect(0, 0, Screen.width, Screen.height * 2 / 100);
+        string caption = string.Format("{0:0.} fps", fps);
+        GUI.Label(rect, caption, Style);
+    }
+}
