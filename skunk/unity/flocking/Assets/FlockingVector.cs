a61a78e9758e3a6b659d5d64701f1874509f3046
Jason Estey <esteyj@yahoo.com>
2018-01-23 16:23:40 -0800

    working on a simple flocking demo, starting with a herd

---
+++ skunk/unity/flocking/Assets/FlockingVector.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+namespace BushidoBurrito
+{
+
+public class FlockingVector : MonoBehaviour
+{
+	public GameObject[] Neighbours;
+
+	public float AlignmentFactor = 1f;
+	public float CohesionFactor = 1f;
+	public float SeparationFactor = 1f;
+
+	public Vector3 Calculate()
+	{
+		Vector3 alignment = Vector3.zero;
+		Vector3 cohesion = Vector3.zero;
+		Vector3 separation = Vector3.zero;
+
+		foreach (var neighbour in Neighbours) {
+			alignment += neighbour.transform.forward;
+			cohesion += neighbour.transform.position;
+			separation += transform.position - neighbour.transform.position;
+		}
+
+		var result = (alignment * AlignmentFactor) +
+			(cohesion * CohesionFactor) +
+			(separation * SeparationFactor);
+
+		return result.normalized;
+	}
+}
+
+} // namespace BushidoBurrito
---
+++ skunk/unity/flocking/Assets/HerdMovement.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+namespace BushidoBurrito
+{
+
+public class HerdMovement : MonoBehaviour
+{
+	public Vector3 BaseMovement = new Vector3(1f, 0f, 0f);
+
+	public float DriftMovement = 0.2f;
+
+	private FlockingVector FlockingVector;
+
+	private void Start()
+	{
+		this.FlockingVector = GetComponent<FlockingVector>();
+
+		BaseMovement.x += Random.Range(-DriftMovement, DriftMovement);
+		BaseMovement.z += Random.Range(-DriftMovement, DriftMovement);
+	}
+
+	private void LateUpdate()
+	{
+		var movement = BaseMovement + this.FlockingVector.Calculate();
+		transform.position = transform.position + movement * Time.deltaTime;
+	}
+}
+
+} // namespace BushidoBurrito
