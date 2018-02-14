49477fad6372fbe06066c6897f175d881f76d843
Jason Estey <esteyj@yahoo.com>
2018-02-13 16:46:01 -0800

    added spinning cube

---
+++ skunk/unity/FrustrumCulling/Assets/Spinner.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+namespace BushidoBurrito
+{
+
+public class Spinner : MonoBehaviour
+{
+	public Vector3 EulerVelocity;
+
+	private void Update()
+	{
+		transform.rotation *= Quaternion.Euler(EulerVelocity * Time.deltaTime);
+	}
+}
+
+} // namespace
