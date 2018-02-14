1a644f08d0c37d323140a712d7cf17d6f450e8e3
Jason Estey <esteyj@yahoo.com>
2018-02-13 17:16:45 -0800

    added basic frustrum culler

---
+++ skunk/unity/FrustrumCulling/Assets/FrustrumCuller.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+namespace BushidoBurrito
+{
+
+public class FrustrumCuller : MonoBehaviour
+{
+	public float ChildRadius = 1f;
+
+	public static bool ContainedByPlanes(Vector3 position, float radius, Plane[] planes)
+	{
+		for (int i = 0; i != planes.Length; i++)
+		{
+			float distance = Vector3.Dot(position, planes[i].normal) + planes[i].distance;
+
+			if (distance < -radius) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private void Update()
+	{
+		var frustrum = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+		foreach (Transform child in this.transform) {
+			child.gameObject.SetActive(ContainedByPlanes(child.position, this.ChildRadius, frustrum));
+		}
+	}
+}
+
+} // namespace
--- skunk/unity/FrustrumCulling/Assets/RadialSpawner.cs
+++ skunk/unity/FrustrumCulling/Assets/RadialSpawner.cs
@@ -8,12 +8,14 @@ public class RadialSpawner : MonoBehaviour
 {
 	public uint SpawnCount;
 	public GameObject Spawnable;
-	public Vector3 Scale;
+	public Transform Parent;
+	public Vector3 Scale = Vector3.one;
 
 	private void Start()
 	{
 		for (uint i = 0; i < this.SpawnCount; i++) {
-			Instantiate(this.Spawnable, Vector3.Scale(Random.insideUnitSphere, this.Scale), Quaternion.identity);
+			var spawn = Instantiate(this.Spawnable, Vector3.Scale(Random.insideUnitSphere, this.Scale), Quaternion.identity) as GameObject;
+			spawn.transform.parent = this.Parent;
 		}
 	}
 }
