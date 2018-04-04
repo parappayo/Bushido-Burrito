a4d2afad2158f2c8a0bdca8072889f0a967f117b
Jason Estey <esteyj@yahoo.com>
2018-04-04 16:04:07 -0700

    added play_note func

--- skunk/python/audio/audio_test.py
+++ skunk/python/audio/audio_test.py
@@ -12,16 +12,24 @@ sample_rate_hz = 44100
 tau = 2 * numpy.pi
 
 def generate_samples(duration_secs, freq_hz):
-	return (numpy.sin(tau * numpy.arange(sample_rate_hz * duration_secs) * freq_hz / sample_rate_hz)).astype(numpy.float32)	
+	return (numpy.sin(tau * numpy.arange(sample_rate_hz * duration_secs) * freq_hz / sample_rate_hz)).astype(numpy.float32)
 
-audio = pyaudio.PyAudio()
+def play_note(freq, volume, duration, stream):
+	stream.write((numpy.clip(volume, 0, 1.0) * generate_samples(duration, freq)).tobytes())
 
-stream = audio.open(format=pyaudio.paFloat32,
-				channels=1,
-				rate=sample_rate_hz,
-				output=True)
+if __name__ == "__main__":
+	audio = pyaudio.PyAudio()
 
-stream.write((volume * generate_samples(1.0, note_freq.A4)).tobytes())
-stream.stop_stream()
-stream.close()
-audio.terminate()
+	stream = audio.open(format=pyaudio.paFloat32,
+					channels=1,
+					rate=sample_rate_hz,
+					output=True)
+
+	play_note(note_freq.A3, 0.2, 1.0, stream)
+	play_note(note_freq.B3, 0.2, 1.0, stream)
+	play_note(note_freq.C4, 0.2, 1.0, stream)
+	play_note(note_freq.D4, 0.2, 1.0, stream)
+
+	stream.stop_stream()
+	stream.close()
+	audio.terminate()
