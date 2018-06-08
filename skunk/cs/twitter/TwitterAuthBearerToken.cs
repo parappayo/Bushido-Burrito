e00d9770778caeb934b59f76686fac49682128ea
Jason Estey <esteyj@yahoo.com>
2018-06-07 17:27:44 -0700

    some samples demonstrating Twitter web API

---
+++ skunk/cs/twitter/TwitterAuthBearerToken.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+using Newtonsoft.Json;
+
+// helpful links
+// https://developer.twitter.com/en/docs/basics/authentication/overview/application-only
+// https://stackoverflow.com/questions/4256136/setting-a-webrequests-body-data
+
+namespace TwitterSample
+{
+    public class TwitterAuthBearerToken
+    {
+        private static string ApiUrl = "https://api.twitter.com/oauth2/token";
+
+        private static WebRequest CreateRequest(string consumerKey, string consumerSecret)
+        {
+            string bearerTokenCredentials = string.Format("{0}:{1}",
+                HttpUtility.UrlEncode(consumerKey),
+                HttpUtility.UrlEncode(consumerSecret));
+
+            string authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(bearerTokenCredentials));
+
+            WebRequest request = WebRequest.Create(ApiUrl);
+            request.Method = "POST";
+            request.Headers["Authorization"] = string.Format("Basic {0}", authorization);
+            request.Headers["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8";
+            return request;
+        }
+
+        private static WebResponse GetResponse(string consumerKey, string consumerSecret)
+        {
+            WebRequest request = CreateRequest(consumerKey, consumerSecret);
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                byte[] postBody = Encoding.UTF8.GetBytes("grant_type=client_credentials");
+                requestStream.Write(postBody, 0, postBody.Length);
+            }
+            return request.GetResponse();
+        }
+
+        private static string ParseResponse(string responseBody)
+        {
+            var responseDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBody);
+
+            if (!responseDictionary["token_type"].Equals("bearer"))
+            {
+                return null;
+            }
+
+            return responseDictionary["access_token"];
+        }
+
+        public static string GetToken(string consumerKey, string consumerSecret)
+        {
+            var response = GetResponse(consumerKey, consumerSecret);
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                byte[] responseBody = new byte[response.ContentLength];
+                long bytesRead = responseStream.Read(responseBody, 0, responseBody.Length);
+
+                if (bytesRead != responseBody.Length)
+                {
+                    return null;
+                }
+
+                return ParseResponse(Encoding.UTF8.GetString(responseBody));
+            }
+        }
+    }
+}
---
+++ skunk/cs/twitter/TwitterUserTimeline.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace TwitterSample
+{
+    public class TwitterUserTimeline
+    {
+        private static string ApiUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+        private static WebRequest CreateRequest(string userName, UInt16 postCount, string bearerToken)
+        {
+            string url = string.Format("{0}?screen_name={1}&count={2}", ApiUrl, userName, postCount);
+
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            request.Headers["Authorization"] = string.Format("Bearer {0}", bearerToken);
+            return request;
+        }
+
+        public static string GetPosts(string userName, UInt16 postCount, string bearerToken)
+        {
+            WebRequest request = CreateRequest(userName, postCount, bearerToken);
+            var response = request.GetResponse();
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                byte[] responseBody = new byte[response.ContentLength];
+                long bytesRead = responseStream.Read(responseBody, 0, responseBody.Length);
+
+                if (bytesRead != responseBody.Length)
+                {
+                    return null;
+                }
+
+                return Encoding.UTF8.GetString(responseBody);
+            }
+        }
+    }
+}
